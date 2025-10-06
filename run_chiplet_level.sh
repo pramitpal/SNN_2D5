51c1e25b286c34b9f72a6821cb3a7080d81517b5
Pramit Kumar Pal <41202066+pramitpal@users.noreply.github.com>
2025-10-06 16:02:38 -0700

    Update run_chiplet_level.sh

--- run_chiplet_level.sh
+++ run_chiplet_level.sh
@@ -2,4 +2,4 @@
 
 chiplet_id=$1
 ./booksim Chiplet_level_config > out_chiplet_${chiplet_id}.txt
-echo "BookSim output saved to out_chiplet_${chiplet_id}.txt"
+echo "BookSim output saved for NoC to out_chiplet_${chiplet_id}.txt"
